ee7b62de46bac39d60074c403921b8d74a4f799e
廖东明 <991192001@qq.com>
2017-02-24 18:06:41 +0800

    init

---
+++ app/src/main/java/app/save/com/saveyourtime/MainActivity.java
@@ -0,0 +1,116 @@
+package app.save.com.saveyourtime;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static int WHITE = 0xFFFFFFFF;
+    public final static int BLACK = 0xFF000000;
+    public final static int WIDTH = 400;
+    public final static int HEIGHT = 400;
+    private String STR ;
+
+    private ImageView imageView;
+    private EditText phoneEditText;
+    //8002|phone|timestamp-后面三位用0来替换|440106B008
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView)findViewById(R.id.imageView);
+        phoneEditText = (EditText)findViewById(R.id.editText);
+
+        String phoneNum = getPhone();
+        if (phoneNum != null && !phoneNum.equals("")) {
+            phoneEditText.setText(phoneNum);
+        }
+
+        imageView.setOnClickListener((l) -> {
+            String phone = phoneEditText.getText().toString();
+            if (phone == null || phone.equals("")) {
+                Toast.makeText(this, "Plz input phone number", Toast.LENGTH_SHORT).show();
+            } else {
+                saveToSharePreference(phone);
+                setBarcode(phone);
+            }
+        });
+    }
+
+    private void saveToSharePreference(String phone) {
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.my_app_share_preference_key), phone);
+        editor.commit();
+    }
+
+    /**
+     *
+     * @return
+     */
+    private String getPhone() {
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        String defaultValue = "";
+        String phone = sharedPref.getString(getString(R.string.my_app_share_preference_key), defaultValue);
+        return phone;
+    }
+
+    private void setBarcode(String phone) {
+        Long tsLong = System.currentTimeMillis();
+        String ts = tsLong.toString();
+        STR = ts.substring(0, ts.length() - 3) + "000";
+        //STR = "8002|phone|timestamp-后面三位用0来替换|440106B008";
+        STR = "8002|" + phone +"|" + STR + "|" + "440106B008";
+        Log.d("loudaer", STR);
+
+        BarcodeFormat barcodeFormat = BarcodeFormat.QR_CODE;
+        Bitmap bitmap = encodeAsBitmap(STR, barcodeFormat, 500, 500);
+        imageView.setImageBitmap(bitmap);
+    }
+
+    protected static Bitmap encodeAsBitmap(String contents,
+                                           BarcodeFormat format, int desiredWidth, int desiredHeight) {
+        final int WHITE = 0xFFFFFFFF;
+        final int BLACK = 0xFF000000;
+
+        MultiFormatWriter writer = new MultiFormatWriter();
+        BitMatrix result = null;
+        try {
+            result = writer.encode(contents, format, desiredWidth,
+                    desiredHeight);
+        } catch (WriterException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        int width = result.getWidth();
+        int height = result.getHeight();
+        int[] pixels = new int[width * height];
+        // All are 0, or black, by default
+        for (int y = 0; y < height; y++) {
+            int offset = y * width;
+            for (int x = 0; x < width; x++) {
+                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+            }
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height,
+                Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        return bitmap;
+    }
+}
